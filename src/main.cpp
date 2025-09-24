b077f5294e102c5474eed03ca39d68aa02f976e8
Lobster <polyak.daavid@gmail.com>
2025-09-24 14:45:00 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -30,36 +30,36 @@ int main(void)
 
 
 	float positions[] = {
-		-0.5, -0.5,
-		 0.5, -0.5,
-		-0.5,  0.5,
-		 0.5,  0.5
+		-0.5f, -0.5f,
+		-0.5f,  0.5f,
+		 0.5f,  0.5f,
+		 0.5f, -0.5f
+	};
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
 	};
 
-	unsigned int vb;
+	unsigned int vb, ib, va;
 	glad_glGenBuffers(1, &vb);
+	glad_glGenBuffers(1, &ib);
+	glad_glGenVertexArrays(1, &va);
+
+	glad_glBindVertexArray(va);
 	glad_glBindBuffer(GL_ARRAY_BUFFER, vb);
+	glad_glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ib);
+
 	glad_glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
+	glad_glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
+	glad_glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 	glad_glEnableVertexAttribArray(0);
-	glad_glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
-	
-	// unsigned int indices[] = {
-	// 	0, 1, 2,
-	// 	2, 3, 0
-	// };
-	//
-	// unsigned int ib;
-	// glad_glGenBuffers(1, &ib);
-	// glad_glBindBuffer(GL_ARRAY_BUFFER, ib);
-	// glad_glBufferData(GL_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glad_glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glad_glDrawArrays(GL_TRIANGLES, 0, 3);
+		glad_glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
