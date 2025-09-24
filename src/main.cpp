053d7f6898031948948e9842cce3e39f400f9475
Lobster <polyak.daavid@gmail.com>
2025-09-24 13:44:52 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,70 @@
-#include <GL/gl.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(void) {
-    GLFWwindow* window;
+int main(void)
+{
+	GLFWwindow* window;
 
-    if (!glfwInit()) return -1;
+	if (!glfwInit()) return -1;
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
+	window = glfwCreateWindow(1280, 720, "Opengl Prject", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	int version = gladLoadGL();
+    if (version == 0) {
+		std::cout << "Failed to initialize OpenGL context\n";
         return -1;
     }
 
-    glfwMakeContextCurrent(window);
-
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(window);
+	float positions[] = {
+		-0.5, -0.5,
+		 0.5, -0.5,
+		-0.5,  0.5,
+		 0.5,  0.5
+	};
 
-        glfwPollEvents();
-    }
+	unsigned int vb;
+	glad_glGenBuffers(1, &vb);
+	glad_glBindBuffer(GL_ARRAY_BUFFER, vb);
+	glad_glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
 
-    glfwTerminate();
-    return 0;
+	glad_glEnableVertexAttribArray(0);
+	glad_glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+	
+	// unsigned int indices[] = {
+	// 	0, 1, 2,
+	// 	2, 3, 0
+	// };
+	//
+	// unsigned int ib;
+	// glad_glGenBuffers(1, &ib);
+	// glad_glBindBuffer(GL_ARRAY_BUFFER, ib);
+	// glad_glBufferData(GL_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glad_glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glad_glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
