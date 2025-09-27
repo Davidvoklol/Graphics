9c2e53fd87c37e994188f63b76b016b48a4dbdbb
Lobster <polyak.daavid@gmail.com>
2025-09-27 12:47:29 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include "GlBuffer.h"
+#include "GlVertexArray.h"
 
 int main(void) {
 	GLFWwindow* window;
@@ -40,16 +41,10 @@ int main(void) {
 		2, 3, 0
 	};
 
-	unsigned int va;
-	glGenVertexArrays(1, &va);
-
-	glBindVertexArray(va);
-
+	GlVertexArray va{};
 	GlBuffer vb(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW, 8);
 	GlBuffer ib(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW, 6);
-
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
+	va.SetLayout(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
